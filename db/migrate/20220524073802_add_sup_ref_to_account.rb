ce6772673c908c5567b9c6b5be3f407d6330eebe
Ankit Ahirrao <aahirrao@bestpeers.com>
2022-05-24 19:32:00 +0530

    files added

---
+++ db/migrate/20220524073802_add_sup_ref_to_account.rb
@@ -0,0 +1,5 @@
+class AddSupRefToAccount < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :accounts, :supplier, null: false, foreign_key: true
+  end
+end
---
+++ db/migrate/20220524074206_remove_account_from_account.rb
@@ -0,0 +1,5 @@
+class RemoveAccountFromAccount < ActiveRecord::Migration[7.0]
+  def change
+    remove_reference :accounts, :account, null: false, foreign_key: true
+  end
+end
